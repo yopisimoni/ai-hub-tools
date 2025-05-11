65d29651cd97f2a2cd708850e271797c6a88009a
Amara Simohamed <simohamed.amara@gmail.com>
2025-05-11 21:32:26 +0000

    // Import the functions you need from the SDKs you need import { initializeApp } from "firebase/app"; import { getAnalytics } from "firebase/analytics"; // TODO: Add SDKs for Firebase products that you want to use // https://firebase.google.com/docs/web/setup#available-libraries
    // Your web app's Firebase configuration
    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
    const firebaseConfig = {
      apiKey: "AIzaSyBc332FpBGBMqrYF2ckseXLJK0thqYfLJk",
      authDomain: "authflow-5fd8d.firebaseapp.com",
      projectId: "authflow-5fd8d",
      storageBucket: "authflow-5fd8d.firebasestorage.app",
      messagingSenderId: "153017336272",
      appId: "1:153017336272:web:f2ba9139cfd7be1081f9d5",
      measurementId: "G-S5T8NKXRCN"
    };
    
    // Initialize Firebase
    const app = initializeApp(firebaseConfig);
    const analytics = getAnalytics(app);

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,19 @@
 // src/lib/firebase.ts
-import { initializeApp, getApps, getApp } from 'firebase/app';
+import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBB_-XFipDOaHqjp1srH-TnniYfnX-7sRc",
+  apiKey: "AIzaSyBc332FpBGBMqrYF2ckseXLJK0thqYfLJk",
   authDomain: "authflow-5fd8d.firebaseapp.com",
   projectId: "authflow-5fd8d",
-  storageBucket: "authflow-5fd8d.appspot.com",
+  storageBucket: "authflow-5fd8d.firebasestorage.app",
   messagingSenderId: "153017336272",
-  appId: "1:153017336272:web:04701f0b0f8f8b0c1c9f7e" // Assuming a web app, you might need to generate this from Firebase console if not already done
+  appId: "1:153017336272:web:f2ba9139cfd7be1081f9d5",
+  measurementId: "G-S5T8NKXRCN"
 };
 
 // Initialize Firebase
-let app;
+let app: FirebaseApp;
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
 } else {
